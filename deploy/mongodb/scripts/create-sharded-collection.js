a3dffc8fec9497913b804e1af84317645177d4b3
Dmitry Kulga <coolgadv@gmail.com>
2017-11-12 13:55:31 +0300

    Even data distribution between shards in JS script that creates sharded collection for MongoDB sharded configuration.

--- deploy/mongodb/scripts/create-sharded-collection.js
+++ deploy/mongodb/scripts/create-sharded-collection.js
@@ -10,8 +10,10 @@ sh.shardCollection("dbWithShardedCollection.shardedCollection", { "shardedField"
 // Fill collection with data. 2M records should be enough to get at least one chunk on both shards.
 // Default max chunk size is 64 Mb.
 var collection = db.getSiblingDB("dbWithShardedCollection").shardedCollection;
-for (index = 0; index < 2000000; index++) {
-    var value = "some_meaningless_string_value_with_number_" + index;
+for (var index = 0; index < 2000000; index++) {
+    var value = index % 2 === 0 ?
+        "some_meaningless_string_value_with_even_number_" + index :
+        "some_meaningless_string_value_with_odd_number_" + index;
     collection.insert({
         shardedField: value
     });
