39200139de008ef0d693a24479a81c376d86eff7
Dmitry Kulga <coolgadv@gmail.com>
2017-11-15 15:01:57 +0300

    Unique index example for single-instance MongoDB.

---
+++ deploy/mongodb/single/scripts/use-unique-index.js
@@ -0,0 +1,31 @@
+// ---
+// This script should be run in mongod shell using the following command:
+// load("/scripts/use-unique-index.js")
+// ---
+
+var collection = db.getSiblingDB("sample").sample;
+collection.drop();
+
+for (var index = 0; index < 1000; index++) {
+    collection.insert({
+        indexedField: index,
+        valueField: "value_" + index
+    });
+}
+
+print("=== Creating index :");
+collection.createIndex({ indexedField: 1 }, { unique: true });
+
+print("=== Collection indexes :");
+printjson(collection.getIndexes());
+
+print("=== Simple query :");
+printjson(collection.find({ indexedField: 500 }).explain({
+    verbosity : "allPlansExecution"
+}));
+
+print("=== Try to insert document with duplicated index field :");
+printjson(collection.insert({
+    indexedField: 500,
+    valueField: "some_value"
+}));
