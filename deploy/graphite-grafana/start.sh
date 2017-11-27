135f0db9a0538d24c6fda0f70703ef879a3b70f5
Dmitry Kulga <coolgadv@gmail.com>
2017-11-27 23:02:38 +0300

    Graphite and Grafana image: port and volume mapping, Docker image layer optimization.

--- deploy/graphite-grafana/start.sh
+++ deploy/graphite-grafana/start.sh
@@ -3,12 +3,13 @@
 . ../common/common.sh
 
 info "Building image ..."
-docker build -t graphite-grafana:1.0 --no-cache ./image
+docker build -t graphite-grafana:1.0 ./image
 
 info "Starting container ..."
 docker run -d --rm -it \
            --name graphite-grafana \
            -p 127.0.0.1:3000:3000 \
+           -p 127.0.0.1:2003:2003 \
            graphite-grafana:1.0
 
 
