3da3edc1cb2dcff541e3fcae34a7e5347f09c842
Dmitry Kulga <coolgadv@gmail.com>
2017-12-18 11:35:22 +0300

    Simple database creation script for single-instance PostgreSQL.

--- deploy/postgres/single/run-shell.sh
+++ deploy/postgres/single/run-shell.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
+docker exec single-postgres rm -rf /scripts/
+
+docker cp scripts single-postgres:/
+
 docker exec -it single-postgres psql -U postgres
---
+++ deploy/postgres/single/scripts/create-database.sql
@@ -0,0 +1,8 @@
+------
+-- to run this script execute the following command in psql console:
+-- \i /scripts/create-database.sql
+------
+
+DROP DATABASE IF EXISTS sample_database;
+
+CREATE DATABASE sample_database;
