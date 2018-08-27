437fe7ba15b8c82e6d3abc9245fbd7760b9c65df
Dmitry Kulga <coolgadv@gmail.com>
2018-08-27 20:46:25 +0300

    Scripts for Artifactory.

---
+++ deploy/artifactory/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+. ../common/common.sh
+
+info "Starting container ..."
+docker run -d --rm -it \
+           --name artifactory \
+           -p 127.0.0.1:8081:8081 \
+           docker.bintray.io/jfrog/artifactory-oss:5.9.1
+
+
