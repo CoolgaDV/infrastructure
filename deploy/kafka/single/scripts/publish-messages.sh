6c698c8e928807c4becc971cdab7dda00dad3b57
Dmitry Kulga <coolgadv@gmail.com>
2019-02-01 08:41:03 +0300

    Message publishing script for single-instance Kafka setup.

---
+++ deploy/kafka/single/scripts/publish-messages.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker exec -it single-kafka-node /bin/bash \
+       /kafka_2.11-2.1.0/bin/kafka-console-producer.sh \
+       --broker-list localhost:9092 \
+       --topic test.topic
