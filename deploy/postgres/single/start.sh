a43b62ee15f22a981290cc1ff744c2730e2345e5
Dmitry Kulga <coolgadv@gmail.com>
2019-03-31 12:47:01 +0300

    Fixed docker image version for single node Postgres setup.

--- deploy/postgres/single/start.sh
+++ deploy/postgres/single/start.sh
@@ -7,7 +7,7 @@ info "Starting docker ..."
 docker run -d --rm -it \
            --name single-postgres \
            -p 127.0.0.1:5656:5432 \
-           postgres:10.1-alpine
+           postgres:10-alpine
 
 info "Waiting for db ..."
 wait_for_db_start single-postgres
